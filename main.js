bc252e9a05d22525907ba04bb9b76547f7960177
Cobald13 <vk2141@student.uni-lj.si>
2023-11-24 20:37:09 +0100

    engine

---
+++ main.js
@@ -0,0 +1,77 @@
+import { ResizeSystem } from './common/engine/systems/ResizeSystem.js';
+import { UpdateSystem } from './common/engine/systems/UpdateSystem.js';
+
+import { OrbitController } from './common/engine/controllers/OrbitController.js';
+import { RotateAnimator } from './common/engine/animators/RotateAnimator.js';
+
+import { GLTFLoader } from './common/engine/loaders/GLTFLoader.js';
+
+import { Renderer } from './Renderer.js'
+
+import { Light } from './Light.js';
+
+import { LinearAnimator } from './common/engine/animators/LinearAnimator.js';
+
+import {
+    Camera,
+    Model,
+    Node,
+    Transform,
+} from './common/engine/core.js';
+
+const canvas = document.querySelector('canvas');
+
+const renderer = new Renderer(canvas);
+await renderer.initialize();
+
+const gltfLoader = new GLTFLoader();
+await gltfLoader.load("common/models/scenaImport/pls.gltf");
+
+const scene = gltfLoader.loadScene(gltfLoader.defaultScene);
+const camera = scene.find(node => node.getComponentOfType(Camera));
+
+camera.addComponent(new OrbitController(camera, document.body, {
+    distance: 8,
+}));
+
+const light = new Node();
+light.addComponent(new Transform({
+    translation: [3, 3, 3],
+}));
+light.addComponent(new Light({
+    ambient: 0.3,
+}));
+//light.addComponent(new LinearAnimator(light, {
+//    startPosition: [3, 3, 3],
+//    endPosition: [-3, -3, -3],
+//    duration: 5,
+//    loop: true,
+//}))
+scene.addChild(light);
+
+const model = scene.find(node => node.getComponentOfType(Model));
+//model.addComponent(new RotateAnimator(model, {
+//    startRotation: [0, 0, 0, 1],
+//    endRotation: [0.7071, 0, 0.7071, 0],
+//    duration: 5,
+//    loop: true,
+//}));
+
+function update(time, dt) {
+    scene.traverse(node => {
+        for (const component of node.components) {
+            component.update?.(time, dt);
+        }
+    });
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
+
+function resize({ displaySize: { width, height } }) {
+    camera.getComponentOfType(Camera).aspect = width / height;
+}
+
+new ResizeSystem({ canvas, resize }).start();
+new UpdateSystem({ update, render }).start();
