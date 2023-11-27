0b1f37ce07ebf1569663ec4c4eb1fb0ae09822db
apogacnik <anzy.pog@gmail.com>
2023-11-27 19:49:05 +0100

    Left/right arrow controlls for bike

--- main.js
+++ main.js
@@ -31,10 +31,21 @@ await gltfLoader.load("common/models/scenaImport/pls.gltf");
 
 const scene = gltfLoader.loadScene(gltfLoader.defaultScene);
 const camera = scene.find(node => node.getComponentOfType(Camera));
+// const camera = gltfLoader.loadNode('Camera');
 
 camera.addComponent(new TurntableController(camera, document.body, {
     distance: 8,
 }));
+camera.getComponentOfType(Camera).far = 1000
+// camera.addComponent(new Transform({
+//     translation: [0, 1, 10],
+// }));
+
+// Example: load from 2nd file
+// const gltfLoader1 = new GLTFLoader();
+// await gltfLoader1.load("common/models/scenaImport/new/tmp/test.gltf");
+// const test = gltfLoader1.loadNode('test');
+// scene.addChild(test)
 
 const light = new Node();
 light.addComponent(new Transform({
@@ -58,7 +69,7 @@ const models = scene.filter(node => node.getComponentOfType(Model));
 models.forEach((model, index) => {
     model.addComponent(new LinearAnimator(model, {
         startPosition: [model.getComponentOfType(Transform).translation[0], model.getComponentOfType(Transform).translation[1], model.getComponentOfType(Transform).translation[2]],
-        endPosition: [model.getComponentOfType(Transform).translation[0], model.getComponentOfType(Transform).translation[1], model.getComponentOfType(Transform).translation[2] + 90],
+        endPosition: [model.getComponentOfType(Transform).translation[0], model.getComponentOfType(Transform).translation[1], model.getComponentOfType(Transform).translation[2] + 20],
         duration: 10,
         startTime: index * 10,
         loop: true,
@@ -88,7 +99,58 @@ models.forEach((model, index) => {
 //    }));
 //}
 
+const bike = gltfLoader.loadNode('Bike');
+// Don't move bike with the scene
+bike.removeComponent(bike.getComponentOfType(LinearAnimator))
+console.log("BIKE:", bike);
+// const startPosition = bike.getComponentOfType(Transform).translation
+// console.log(startPosition)
+
+// Controls: Left/Right arrow pressed
+// (linear animator doesn't work...)
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft' || event.code === 'ArrowLeft') {
+        console.log("Left arrow key pressed!");
+        
+        // linearAnimatorComponent.startPosition = [bike.getComponentOfType(Transform).translation[0], bike.getComponentOfType(Transform).translation[1], bike.getComponentOfType(Transform).translation[2], 1];
+        // linearAnimatorComponent.endPosition = [bike.getComponentOfType(Transform).translation[0], bike.getComponentOfType(Transform).translation[1], bike.getComponentOfType(Transform).translation[2]-1, 1];
+        // linearAnimatorComponent.duration = 5;
+
+        // bike.addComponent(new LinearAnimator(bike, {
+        //     startPosition: [bike.getComponentOfType(Transform).translation[0], bike.getComponentOfType(Transform).translation[1], bike.getComponentOfType(Transform).translation[2], 1],
+        //     endPosition: [bike.getComponentOfType(Transform).translation[0]-1, bike.getComponentOfType(Transform).translation[1], bike.getComponentOfType(Transform).translation[2], 1],
+        //     duration: 5,
+        // }))
+        // bike.removeComponent(bike.getComponentOfType(LinearAnimator))
+        // console.log("linear animators:", bike.getComponentsOfType(LinearAnimator));
+
+        bike.getComponentOfType(Transform).translation[0] += -1
+    } else if (event.key === 'ArrowRight' || event.code === 'ArrowRight') {
+        console.log("Right arrow key pressed!");
+
+        // bike.addComponent(new LinearAnimator(bike, {
+        //     startPosition: [bike.getComponentOfType(Transform).translation[0], bike.getComponentOfType(Transform).translation[1], bike.getComponentOfType(Transform).translation[2], 1],
+        //     endPosition: [bike.getComponentOfType(Transform).translation[0]+1, bike.getComponentOfType(Transform).translation[1], bike.getComponentOfType(Transform).translation[2], 1],
+        //     duration: 5,
+        // }))
+        // bike.removeComponent(bike.getComponentOfType(LinearAnimator))
+
+        bike.getComponentOfType(Transform).translation[0] += 1
+    }  else if (event.key === 'ArrowUp' || event.code === 'ArrowUp') {
+
+        // Camera transformations - testing (remove TurntableController component)
+        camera.getComponentOfType(Transform).translation[2] -= 1;
+        camera.getComponentOfType(Camera).fovy = 1.5;
+        console.log(camera);
+    }
+});
+
+
 function update(time, dt) {
+    // Move bike & camera forwards
+    // bike.getComponentOfType(Transform).translation[2] -= dt*3
+    // camera.getComponentOfType(Transform).translation[2] -= dt*3
+
     scene.traverse(node => {
         for (const component of node.components) {
             component.update?.(time, dt);
